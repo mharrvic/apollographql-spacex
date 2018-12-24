3bf648a7d21d8399a3a721ef6d4703a6b82f62cd
mharrvic <mharrvic@gmail.com>
2018-12-24 18:44:16 +0800

    updated local state

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,44 @@
-import { ApolloProvider } from "react-apollo";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Query, ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
+import Pages from "./pages";
+import Login from "./pages/login";
+import { resolvers, typeDefs } from "./resolvers";
+import injectStyles from "./styles";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+    headers: {
+      authorization: localStorage.getItem("token")
+    }
+  }),
+  initializers: {
+    isLoggedIn: () => !!localStorage.getItem("token"),
+    cartItems: () => []
+  }
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <Query query={IS_LOGGED_IN}>
+      {({ data }) => (data.isLoggedIn ? <Pages /> : <Login />)}
+    </Query>
   </ApolloProvider>,
   document.getElementById("root")
 );
